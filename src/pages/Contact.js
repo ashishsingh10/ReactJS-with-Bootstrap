db2e152ffd53ec516c0d27648d1fa2d489968a04
Ashish Singh <ashish.singh24785@gmail.com>
2023-09-23 19:25:58 +0530

    first commit

---
+++ src/pages/Contact.js
@@ -0,0 +1,11 @@
+const Contact = () => {
+    return (
+        <>
+            <div className="alert alert-primary" role="alert">
+                <h1>Contact</h1>
+            </div>
+        </>
+    )
+}
+
+export default Contact;
